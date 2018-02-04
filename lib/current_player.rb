f56c86dd5e62979fd4f431e431f1c1eee9f3a2a1
Chely Ho <adept_inept@yahoo.com>
2018-02-04 04:09:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,11 @@
 def turn_count(board)
   count = 0
   board.each do |space|
-   if is_occupied?(space)
+   if space == "X" || space == "O"
      count += 1
    end
   end
  count
 end
 
-def is_occupied?(space)
-  space == "X" || space =="O" ? true : false
-end
+
